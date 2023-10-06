c1e2a9fbedc2264d9aa688ae38b76528be98c928
Cauã Lucas <cauallucas71@gmail.com>
2023-10-06 16:45:54 -0300

    commit

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ function fecharPopup() {
 setTimeout(mostrarPopup, 2000);
 
 
-
-
-
+$(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true
+  });
+  $(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+      $(".custom-carousel .item").not($(this)).removeClass("active");
+      $(this).toggleClass("active");
+    });
+  });
+  
