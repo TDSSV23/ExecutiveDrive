d5e4b1c863904158a4d8f399f6d8ed9202622bf4
Cauã Lucas <cauallucas71@gmail.com>
2023-11-01 10:12:58 -0300

    comitt

--- script.js
+++ script.js
@@ -22,3 +22,21 @@ $(".custom-carousel").owlCarousel({
     });
   });
   
+  const myModal = document.getElementById('myModal')
+  const myInput = document.getElementById('myInput')
+  
+  myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+  })
+
+
+  $(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true
+  });
+  $(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+      $(".custom-carousel .item").not($(this)).removeClass("active");
+      $(this).toggleClass("active");
+    });
+  });
